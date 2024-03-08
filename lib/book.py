c4572d405e2d2012f0412af48f53b53a630cac6e
Cheruiyot Kigen David <cheruiyotscripts@gmail.com>
2024-03-08 17:54:25 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
-
 class Book:
-    
-    def __init__(self,title,page_count):
+    def __init__(self, title, page_count):
         self.title = title
-        self.set_page_count(page_count)
-        
-    def set_page_count(self, page_count):
-        if not isinstance(page_count, int):
+        self._page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
             print("page_count must be an integer")
         else:
-            self.page_count = page_count
-        
+            self._page_count = value
+
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
-    
-        
-    
-   
-# book1=Book("Book", 'andrew')
-
-# print(book1.page_count)
