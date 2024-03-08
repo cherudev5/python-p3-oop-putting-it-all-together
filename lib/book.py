94e18ef215a250b4d61fda387eb79ed2d35775d1
Cheruiyot Kigen David <cheruiyotscripts@gmail.com>
2024-03-08 16:13:38 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    
+    def __init__(self,title,page_count):
+        self.title = title
+        self.set_page_count(page_count)
+        
+    def set_page_count(self, page_count):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+        else:
+            self.page_count = page_count
+        
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
         
+    
+   
+# book1=Book("Book", 'andrew')
+
+# print(book1.page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+       self.brand = brand
+       self.size = size
+       self.condition = "Used"
+        
+  
+    def set_size(self, size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+        else:
+            self.size = size
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New" 
+      
